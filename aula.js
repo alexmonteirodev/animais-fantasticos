4e570e07a2af6cd9c40fe3ecda7453ecd16589bd
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-08 14:51:40 +0200

    Update aula.js

--- aula.js
+++ aula.js
@@ -13,11 +13,67 @@
 // 8 - cria o metodo de adicionar o evento (addTabNavEvent)
 // 9 - faz a verificação do if no init pro código só rodar se for ser usado (se tiver legth)
 
-
 //Ex:
 
 // Antes:
 
+// export default function initTabNav() {
+//     //essa função serve para isolar o escopo
+//     const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+//     const tabContent = document.querySelectorAll('[data-tab="content"] section');
+
+//     function activeTab(index) {
+//       tabContent.forEach((params) => {
+//         params.classList.remove("ativo");
+//       });
+//       const direcao = tabContent[index].dataset.anime;
+//       tabContent[index].classList.add("ativo", direcao);
+//     }
+
+//     if (tabMenu.length && tabContent.length) {
+//       //if para confirmar se o item selecionado (.js-tabcontent section) existe = !!tabMenu.length = true && !!tabContent.length = true porque sao numeros maiores que 0 (0 = false)
+
+//       tabMenu.forEach((img, index) => {
+//         //console.log(img, index)
+//         img.addEventListener("click", function name(params) {
+//           activeTab(index);
+//         });
+//         //o problema é que após isso, ao abrir a pagina o conteudo esta em branco e para deixar um texto inicial da raposa basta adicionar a class ativo na section da raposa ou fazer por js da seguinte forma:
+//         tabContent[0].classList.add("ativo");
+//       });
+//     }
+//   }
+
 //Depois:
+
+// export default class TabNav {
+//     constructor(menu, conteudo) {
+//       this.tabMenu = document.querySelectorAll(menu);
+//       this.tabContent = document.querySelectorAll(conteudo);
+//       this.activeClass = "ativo";
+//     }
+//     //ativa a tab de acordo com o index da mesma
+//     activeTab(index) {
+//       this.tabContent.forEach((params) => {
+//         params.classList.remove(this.activeClass);
+//       });
+//       const direcao = this.tabContent[index].dataset.anime;
+//       this.tabContent[index].classList.add(this.activeClass, direcao);
+//     }
+
+//     //adiciona os eventos nas tabs
+//     addTabNavEvent() {
+//       this.tabMenu.forEach((img, index) => {
+//         img.addEventListener("click", () => this.activeTab(index));
+//       });
+//     }
+
+//     init() {
+//       if (this.tabMenu.length && this.tabContent.length) {
+//         this.activeTab([0]); //ativa o primeiro item
+//         this.addTabNavEvent();
+//       }
+//     }
+//   }
 
 //essa aula é a de refatorar tabnav - 1104;
